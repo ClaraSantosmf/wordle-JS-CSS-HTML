9d717689df30fdd9530b1aece077f9897d2a6a5c
ClaraSantosmf <clarasantosmf@gmail.com>
2022-08-17 20:15:37 -0300

    alterações

--- script/wordle.js
+++ script/wordle.js
@@ -6,9 +6,9 @@ let linha = 1
 
 let entrada = []
 
+let JogoAcabou = false // construa e use esse script//
 
-const ouvinteDeTeclas = (event) => {
-
+function ouvinteDeTeclas (event) {
     let char = event.key.toUpperCase();
     let alfabeto = [
         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
@@ -20,31 +20,50 @@ const ouvinteDeTeclas = (event) => {
     }
 
     if (char == 'ENTER') {
-        validarEntrada()
+       if (!validarEntrada()) return; 
         linha += 1
         return;
     }
 
     if (char == 'BACKSPACE'){
-        entrada.pop()
-        console.log(entrada)
+        if (entrada.length !=0 ){
+            let elId = `l${linha}c${entrada.length}`
+            const el = document.getElementById(elId)
+            el.textContent = ''
+            entrada.pop();
+        }
         return;
     }
-
-    entrada.push(char)
-    console.log(entrada)
-
-    exibeLetra(char)
+    if (entrada.length < 5){
+        entrada.push(char)  
+    }
+    if (entrada.length <= 5){
+        exibeLetra()  
+    }
 }
 
 function exibeLetra(letra) {
     let elId = `l${linha}c${entrada.length}`
     const el = document.getElementById(elId)
-    el.textContent = letra
+    el.textContent = entrada[entrada.length-1]
 }
 
 function validarEntrada() {
     console.log('validar se ' + entrada + ' é igual ' + palavraDoDia)
+    if (entrada.length != 5){
+        alert('Precisamos de uma palavra de 5 letras')
+        return false
+    }
+    else{
+        if (entrada.join('') == palavraDoDia.toUpperCase()){
+            alert("Acertou!")
+        document.getElementsByClassName(`linha-tab linha-tab${linha}`).toggle("correct")
+    
+    }
+        entrada=[]
+        return true 
+    }
+
 }
 
 
